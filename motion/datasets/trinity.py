e49a0ec94c343ad05291fb0b3b29b11af41c5c4c
Simon Alexanderson <simonal@kth.se>
2020-08-03 13:49:14 +0200

    added bvh-conversion to generated output

--- motion/datasets/trinity.py
+++ motion/datasets/trinity.py
@@ -1,9 +1,14 @@
 import os
 import sys
 import numpy as np
+import joblib as jl
 from .motion_data import MotionDataset, TestDataset
 from torch.utils.data import Dataset
 from sklearn.preprocessing import StandardScaler
+
+module_path = os.path.abspath(os.path.join('data_processing'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
 
 def fit_and_standardize(data):
 
@@ -40,6 +45,9 @@ class Trinity():
         #use this to generate visualizations for network tuning. It contains the same data as val_input, but sliced into longer 20-sec exerpts
         test_input = np.load(os.path.join(data_root, 'dev_input_'+str(hparams.Data.framerate)+'fps.npz'))['clips'].astype(np.float32)
         
+        #load pipeline for convertion from motion features to BVH.
+        self.data_pipe = jl.load(os.path.join(data_root, 'data_pipe_'+str(hparams.Data.framerate)+'fps.sav'))
+        
         #use this to generate test data for evaluation
         #test_input = np.load(os.path.join(data_root, 'test_input_'+str(hparams.Data.framerate)+'fps.npz'))['clips'].astype(np.float32)
                        
@@ -61,12 +69,24 @@ class Trinity():
         self.validation_dataset = MotionDataset(val_input, val_output, hparams.Data.seqlen, hparams.Data.n_lookahead, hparams.Data.dropout)    
         self.test_dataset = TestDataset(test_input, test_output)
         
-        # Stor scaler
+        # Store scaler and fps
         self.scaler = output_scaler
+        self.fps = hparams.Data.framerate
 
     def save_animation(self, control_data, motion_data, filename):
         anim_clips = inv_standardize(motion_data[:(self.n_test),:,:], self.scaler)
         np.savez(filename + ".npz", clips=anim_clips)  
+        write_bvh(anim_clips, filename, )
+        
+    def write_bvh(self, anim_clips, filename):
+        print('inverse_transform...')
+        inv_data=self.data_pipe.inverse_transform(anim_clips)
+        writer = BVHWriter()
+        for i in range(0,anim_clips.shape[0]):
+            filename_ = f'{filename}_{str(i)}.bvh'
+            print('writing:' + filename_)
+            with open(filename_,'w') as f:
+                writer.write(inv_data[i], f, framerate=self.fps)
         
     def get_train_dataset(self):
         return self.train_dataset
