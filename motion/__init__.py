da545672de2804b40c9c8d6a3acb86fdec7cb63c
Simon Alexanderson <simonal@kth.se>
2020-12-12 12:36:05 +0100

    added locomotion_rot

--- motion/__init__.py
+++ motion/__init__.py
@@ -1,6 +1,7 @@
-from .datasets import Locomotion, Trinity
+from .datasets import Locomotion, Locomotion_rot, Trinity
 
 Datasets = {
     "locomotion": Locomotion,
+    "locomotion_rot": Locomotion_rot,
     "trinity": Trinity
 }
